d167b208ae870da9e751bc201db5c8ae2bf76f3f
hy01it <huyenn5200@gmail.com>
2026-01-29 22:23:00 +0700

    Add Ex5.py with basic variable operations

---
+++ chapter1/Ex5.py
@@ -0,0 +1,16 @@
+name = "Huyen"
+age = 18
+average_score = 9.4
+
+print(type(name))
+print(type(age))
+print(type(average_score))
+
+age_next_year = age + 1
+doubled_score = average_score * 2
+
+print("Tên:", name)
+print("Tuổi:", age)
+print("Tuổi năm sau:", age_next_year)
+print("Điểm TB:", average_score)
+print("Điểm nhân đôi:", doubled_score)
