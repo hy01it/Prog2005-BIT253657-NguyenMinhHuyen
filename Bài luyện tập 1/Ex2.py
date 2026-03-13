84b5ca825ab4d5cf6d5ae20c58d66e251a115a99
hy01it <huyenn5200@gmail.com>
2026-03-13 17:31:28 +0700

    Create Ex2.py

---
+++ Bài luyện tập 1/Ex2.py
@@ -0,0 +1,8 @@
+import math
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+print("Lũy thừa:", a**b)
+print("Căn bậc 2 của a:", math.sqrt(a))
+print("Chia lấy phần nguyên:", a // b)
+print("Chia lấy phần dư:", a % b)
+print("Làm tròn a:", round(a))
