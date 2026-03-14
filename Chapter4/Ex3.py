726b46f81b24f8fed49d161ba5df66fb613b303d
hy01it <huyenn5200@gmail.com>
2026-03-14 22:24:21 +0700

    Create Ex3.py

---
+++ Chapter4/Ex3.py
@@ -0,0 +1,9 @@
+students = {
+    "Nam": 8,
+    "Lan": 9,
+    "Huy": 7}
+key = input("Nhập tên cần kiểm tra: ")
+if key in students:
+    print("Key tồn tại trong dictionary")
+else:
+    print("Key không tồn tại")
