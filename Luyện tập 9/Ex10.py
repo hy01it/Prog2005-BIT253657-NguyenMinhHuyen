69c4820b78862055533dcf70a885d511c391680d
hy01it <huyenn5200@gmail.com>
2026-03-16 18:14:24 +0700

    Create Ex10.py

---
+++ Luyện tập 9/Ex10.py
@@ -0,0 +1,10 @@
+class SinhVien:
+    def __init__(self, diem):
+        self.diem = diem
+    def __eq__(self, other):
+        return self.diem == other.diem
+sv1 = SinhVien(8)
+sv2 = SinhVien(8)
+sv3 = SinhVien(9)
+print("sv1 == sv2:", sv1 == sv2)
+print("sv1 == sv3:", sv1 == sv3)
