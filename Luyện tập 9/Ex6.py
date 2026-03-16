30feee9bc46bf5870083bd1217f6dd177d2e60e8
hy01it <huyenn5200@gmail.com>
2026-03-16 17:57:59 +0700

    Create Ex6.py

---
+++ Luyện tập 9/Ex6.py
@@ -0,0 +1,16 @@
+class Product:
+    def __init__(self, price):
+        self.price = price
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self._price = value
+        else:
+            print("Giá phải > 0")
+    def __str__(self):
+        return f"Price: {self._price}"
+p = Product(150)
+print(p)
