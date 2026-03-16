eba71a50b808cbc8a4f02106a2dd9549e37abe33
hy01it <huyenn5200@gmail.com>
2026-03-16 17:58:25 +0700

    Create Ex7.py

---
+++ Luyện tập 9/Ex7.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    @classmethod
+    def from_string(cls, data):
+        name, age = data.split("-")
+        return cls(name, int(age))
+p = Person.from_string("Nam-20")
+print(p.name, p.age)
