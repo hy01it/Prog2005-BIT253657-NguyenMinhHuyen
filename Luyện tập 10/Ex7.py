ce7cf91f66b8f4cb14bad4f7b58627902e00a2f1
hy01it <huyenn5200@gmail.com>
2026-03-20 18:05:19 +0700

    Create Ex7.py

---
+++ Luyện tập 10/Ex7.py
@@ -0,0 +1,7 @@
+    while True:
+        pw = input("Nhập mật khẩu: ")
+        if pw == "python123":
+            print("Đúng mật khẩu")
+            break
+        else:
+            print("Sai, nhập lại")
