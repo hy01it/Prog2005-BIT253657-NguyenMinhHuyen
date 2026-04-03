fd4e76c19ab44b6ab66e8f95bb4eb39ef56767db
hy01it <huyenn5200@gmail.com>
2026-04-03 18:13:54 +0700

    Update Ex4.py

--- Luyện tập 14/Ex4.py
+++ Luyện tập 14/Ex4.py
@@ -2,13 +2,18 @@ class Book:
     def __init__(self, name, price):
         self._name = name
         self._price = price
-    def get_name(self):
-        return self._name
-    def set_name(self, name):
-        self._name = name
-    def get_price(self):
-        return self._price
-    def set_price(self, price):
-        self._price = price
-book = Book("Python", 30000)
-print("Giá sách:", book.get_price())
+
+    @property
+    def name(self): return self._name
+
+    @name.setter
+    def name(self, value): self._name = value
+
+    @property
+    def price(self): return self._price
+
+    @price.setter
+    def price(self, value): self._price = value
+
+my_book = Book("Lập trình Python", 50000)
+print(f"Giá của cuốn sách: {my_book.price}")
